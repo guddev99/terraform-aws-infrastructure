f86f8fdc6fc4977a3ab409a9d767fb43e1dbbed1
biswaraj <biswaraj1999@gmail.com>
2024-04-13 19:19:35 +0530

    Added vpc.tf

---
+++ vpc.tf
@@ -0,0 +1,33 @@
+# Creates a VPC
+resource "aws_vpc" "tf-vpc" {
+  cidr_block       = var.vpc_cidr_block
+  instance_tenancy = default
+
+  tags = {
+    Name = "tf-vpc"
+  }
+}
+
+# Creates Internet Gateway
+resource "aws_internet_gateway" "tf-IG" {
+  vpc_id = aws_vpc.tf-vpc.id
+
+  tags = {
+    Name = "tf-IG"
+  }
+}
+
+# Creates Custom Route Table
+resource "aws_route_table" "tf-RT" {
+  vpc_id = aws_vpc.tf-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.tf-IG.id
+  }
+
+  tags = {
+    Name = "tf-RT"
+  }
+}
+
