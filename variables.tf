3189f7e845958398612bf23540807f5ea5249fd1
biswaraj <biswaraj1999@gmail.com>
2024-04-13 20:41:03 +0530

    Added variables.tf

---
+++ variables.tf
@@ -0,0 +1,51 @@
+###############################
+# Provider variables
+###############################
+variable "aws_region" {
+  description = "The AWS region"
+  default     = "us-east-1"
+}
+
+###############################
+# VPC variables
+###############################
+variable "vpc_cidr_block" {
+  description = "The CIDR block for the VPC"
+  default     = "10.0.0.0/16"
+}
+
+###############################
+# Subnet variables
+###############################
+variable "subnet_cidr_block_1" {
+  description = "The CIDR block for subnet 1"
+  default     = "10.0.1.0/24"
+}
+
+variable "subnet_cidr_block_2" {
+  description = "The CIDR block for subnet 2"
+  default     = "10.0.2.0/24"
+}
+
+variable "availability_zone_1" {
+  description = "The availability zone for subnet 1"
+  default     = "us-east-1a"
+}
+
+variable "availability_zone_2" {
+  description = "The availability zone for subnet 2"
+  default     = "us-east-1b"
+}
+
+###############################
+# Launch Template variables
+###############################
+variable "image_id" {
+  description = "The ID of the AMI to use for instances"
+  default     = ""
+}
+
+variable "key_name" {
+  description = "The name of the SSH key pair to use for instances"
+  default     = "test-key"
+}
